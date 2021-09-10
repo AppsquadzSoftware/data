a57e970c0b3b2a1408cbdc42debf2522496ed0fe
AppsquadzSoftware <61544855+AppsquadzSoftware@users.noreply.github.com>
2021-09-10 17:00:40 +0530

    Add files via upload
    msg

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiServer: 'https://pokeapi.co/api/v2/',
+  api:{
+    pokemon:'pokemon',
+  },
+  role:{
+    site_admin:[],
+    admin:[],
+    user:[]
+  }
+};
